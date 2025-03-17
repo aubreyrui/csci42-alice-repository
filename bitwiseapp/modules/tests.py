8949fead8c17c617e4f4cdc285013d61e60b8083
irishcreamcb <157715369+irishcreamcb@users.noreply.github.com>
2025-03-17 23:05:17 +0800

    module feature branch created. began work on testcases.

--- bitwiseapp/modules/tests.py
+++ bitwiseapp/modules/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import ModuleCategory, Module, Comment
+from accounts.models import Profile 
+
+
+class ModuleCategoryTest(TestCase): 
+    @classmethod 
+    def setUpTestData(cls):
+        return 0
+
+class ModuleTest(TestCase): 
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User.objects.create_user(username='test_user', password='12345@BbBb')
+        Profile.objects.create(user=user1, display_name='Bob', email='name@test.com')
+        
