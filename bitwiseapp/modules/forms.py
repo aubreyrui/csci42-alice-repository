046bfa08be41a4d747798bb369ad9cf7ec225c30
michael-gdc <michael.delcastillo@student.ateneo.edu>
2025-04-10 13:51:48 +0800

    styled module creation page

--- bitwiseapp/modules/forms.py
+++ bitwiseapp/modules/forms.py
@@ -1,17 +1,27 @@
 from django import forms
 
-from .models import Module, Comment, Gallery
+from .models import Comment, Gallery, Module
 
 
 class ModuleForm(forms.ModelForm):
     class Meta:
         model = Module
-        fields = ["title", "entry", "category", ]
+        fields = [
+            "title",
+            "entry",
+            "category",
+        ]
+
+    template_name = "module_create_snippet.html"
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ["entry",]
+        fields = [
+            "entry",
+        ]
+
 
 class GalleryForm(forms.ModelForm):
     class Meta:
